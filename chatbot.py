710f7b60bd64144e6f100c469ba7791bff089890
psnagra <psnagra13@gmail.com>
2018-07-01 02:31:45 +0900

    First Modification

--- chatbot.py
+++ chatbot.py
@@ -6,20 +6,28 @@ from library.response.getResponse import getResponse
 class chatbot:
 
     def __init__(self,chatbotName = "Jarvis" ,userName= "Tony Stark"):
-        self.chatbotName = chatbotName
-        self.userName = userName
+        self.variables = {
+            'chatbotName' : chatbotName ,
+            'userName' : userName
+        }
 
     # A function that responds to a user's message: respond
     def respond(self,message):
         # Concatenate the user's message to the end of a standard bot respone
         bot_message = getResponse (message)
+        bot_message = self.formatResponse(bot_message)
         # Return the result
         return bot_message
 
+    def formatResponse (self , response):
+        for key in self.variables.keys():
+            response = response.replace('##' + key, self.variables[key])
+        return response
+
     def printMessageAndResponse (self,message, response):
 
-        print userResponseFormat(self.userName,message)
-        print chatbotResponseFormat(self.chatbotName,response)
+        print userResponseFormat(self.variables['userName'],message)
+        print chatbotResponseFormat(self.variables['chatbotName'],response)
 
 
 
@@ -27,6 +35,12 @@ if __name__ == "__main__":
 
     testbot = chatbot()
     message = "Hello Chatbot "
+    response = testbot.respond (message)
+    testbot.printMessageAndResponse (message ,response)
+    print "\n*********************************************************"
+    print "*********************************************************\n"
+
+    message = "what's your name?"
     response = testbot.respond (message)
     testbot.printMessageAndResponse (message ,response)
     print "\n*********************************************************"
